3ccb2d50399dde85ef6759e98a8f63ef6f8dbc3d
jarredb9 <jarred.byrnes@gmail.com>
2025-09-19 09:58:49 -0400

    add logs to troubleshoot favorite and want to go buttons

--- components/winery-modal.tsx
+++ components/winery-modal.tsx
@@ -303,12 +303,14 @@ export default function WineryModal({ winery, onClose, onSaveVisit, onUpdateVisi
   };
 
   const handleWishlistToggle = async () => {
+    console.log('onToggleWishlist prop:', onToggleWishlist);
     setWishlistLoading(true);
     await onToggleWishlist(internalWinery, !!internalWinery.onWishlist);
     setWishlistLoading(false);
   };
   
   const handleFavoriteToggle = async () => {
+    console.log('onToggleFavorite prop:', onToggleFavorite);
     setFavoriteLoading(true);
     await onToggleFavorite(internalWinery, !!internalWinery.isFavorite);
     setFavoriteLoading(false);
