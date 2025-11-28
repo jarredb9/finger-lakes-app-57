49adf4b313c7edf93206d203f160cde6054b521b
Jarred Byrnes <jarred.byrnes@gmail.com>
2025-11-28 12:48:27 -0500

    chore: cleanup scripts directory

--- scripts/lint-staged.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-files=""
-for file in "$@"; do
-  files="$files --file $file"
-done
-
-# Run next lint with the constructed file arguments
-# We use 'exec' so the exit code is preserved
-exec npx next lint --fix $files
