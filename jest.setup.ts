d6a2f1b96a165eb566c879341721696a38b71541
Jarred Byrnes <jarred.byrnes@gmail.com>
2025-12-01 11:23:31 -0500

    fix: Resolve type errors in jest.setup.ts by casting node-fetch polyfills to any

--- jest.setup.ts
+++ jest.setup.ts
@@ -7,13 +7,13 @@ global.TextDecoder = TextDecoder as any;
 // Polyfill Request, Response, Headers for Next.js App Router tests in JSDOM
 // Next.js 14+ relies on these globals for server components/actions.
 if (typeof global.Request === 'undefined') {
-  global.Request = class Request extends (require('node-fetch').Request) {};
+  global.Request = (require('node-fetch').Request) as any;
 }
 if (typeof global.Response === 'undefined') {
-  global.Response = class Response extends (require('node-fetch').Response) {};
+  global.Response = (require('node-fetch').Response) as any;
 }
 if (typeof global.Headers === 'undefined') {
-  global.Headers = class Headers extends (require('node-fetch').Headers) {};
+  global.Headers = (require('node-fetch').Headers) as any;
 }
 
 // Mock the 'next/cache' functions that are used by server actions
