ab36381d7fe227317640c9d4917332dc50c18a7e
Jarred Byrnes <jarred.byrnes@gmail.com>
2025-12-01 12:51:34 -0500

    fix: Correct SQL syntax for ALTER POLICY in migration

--- supabase/migrations/20251201000009_restrict_profile_rls.sql
+++ supabase/migrations/20251201000009_restrict_profile_rls.sql
@@ -4,4 +4,4 @@
 ALTER POLICY "Public profiles are viewable by everyone." ON public.profiles RENAME TO "Users can view their own and their friends' profiles";
 
 ALTER POLICY "Users can view their own and their friends' profiles" ON public.profiles
-FOR SELECT USING (auth.uid() = id OR id IN (SELECT friend_id FROM get_friends_ids()));
+USING (auth.uid() = id OR id IN (SELECT friend_id FROM get_friends_ids()));
