8f767bed093c4fa6e777d139028b410a0f35a4bf
Jarred Byrnes <jarred.byrnes@gmail.com>
2025-12-01 11:13:21 -0500

    fix: Resolve test cases failing
    TextEncoder is not defined in the Jest environment (jsdom). This is a common issue when testing Next.js applications that use  server-side features (which rely on Node.js globals) in a jsdom environment.
    
    Fixed this by adding TextEncoder and TextDecoder to the global scope in jest.setup.ts.

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,3 +3,21 @@ import { TextEncoder, TextDecoder } from 'util';
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder as any;
+
+// Polyfill Request, Response, Headers for Next.js App Router tests in JSDOM
+// Next.js 14+ relies on these globals for server components/actions.
+if (typeof global.Request === 'undefined') {
+  global.Request = class Request extends (require('node-fetch').Request) {};
+}
+if (typeof global.Response === 'undefined') {
+  global.Response = class Response extends (require('node-fetch').Response) {};
+}
+if (typeof global.Headers === 'undefined') {
+  global.Headers = class Headers extends (require('node-fetch').Headers) {};
+}
+
+// Mock the 'next/cache' functions that are used by server actions
+jest.mock('next/cache', () => ({
+  revalidatePath: jest.fn(),
+  revalidateTag: jest.fn(),
+}));
