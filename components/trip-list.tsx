5089d72c1d1f1afbfc5d0b51e9df5607688ad3bc
Jarred Byrnes <jarred.byrnes@gmail.com>
2025-12-05 11:50:36 -0500

    Fix: TripList additional grid fix for large screen

--- components/trip-list.tsx
+++ components/trip-list.tsx
@@ -48,7 +48,7 @@ export default function TripList() {
                 </Button>
             </div>
             {trips.length > 0 ? (
-                <div className="grid gap-4 lg:grid-cols-3 w-full">
+                <div className="grid gap-4 w-full">
                     {trips.map((trip: Trip) => (
                         <TripCardSimple key={trip.id} trip={trip} onDelete={handleDeleteTrip} />
                     ))}
