71c4c19751cbaf5340775113b17d9f947c80e718
Jarred Byrnes <jarred.byrnes@gmail.com>
2025-12-09 13:20:19 -0500

    Fix: Add missing DialogDescription import in WineryModal
    - Corrected 'components/winery-modal.tsx' to explicitly import 'DialogDescription' from '@/components/ui/dialog'. This resolves a 'Cannot find name' TypeScript error that prevented the build from completing after adding the accessible dialog description.

--- components/winery-modal.tsx
+++ components/winery-modal.tsx
@@ -1,6 +1,6 @@
 // components/winery-modal.tsx
 import { useState, useEffect, useRef } from "react";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog";
 import { Separator } from "./ui/separator";
 import Link from "next/link";
 import { Badge } from "@/components/ui/badge";
